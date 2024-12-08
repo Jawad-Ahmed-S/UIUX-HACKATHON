63e116f05f1a6d0c81cfe1883ad16549387c124e
Jawad Ahmed <soomrojawad1.2.2006@gmail.com>
2024-12-08 23:40:47 +0500

    ..

--- app/Components/mainPage/serviceTile.tsx
+++ app/Components/mainPage/serviceTile.tsx
@@ -2,7 +2,7 @@ import { DeliveryIcon } from "../icons";
 
 function ServiceTile({ title, description, Icon }) {
   return (
-    <div className="bg-custom-gray-light flex justify-center items-center w-[18rem] md:flex-grow h-[15rem] md:w-[90%] md:h-auto">
+    <div className="bg-custom-gray-light flex justify-center items-center md:w-[18rem] md:flex-grow md:h-[15rem] w-[90%] h-auto">
       <div className="pt-[3rem] pb-[3rem] ml-[3rem] mr-[3rem]">
         <Icon className="w-[1.5rem] mb-4" />
         <h1 className="headline-four text-custom-purple-dark mb-4">{title}</h1>
