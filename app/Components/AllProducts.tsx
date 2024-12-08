ab8ee318bf4375b4e77e244c9ddb0cf08d5766a3
Jawad Ahmed <soomrojawad1.2.2006@gmail.com>
2024-12-08 23:22:27 +0500

    final

--- app/Components/AllProducts.tsx
+++ app/Components/AllProducts.tsx
@@ -1,11 +1,14 @@
 import { productDataAll } from "../Info";
 import ProductCard from "./Product";
 import { ButtonGray } from "./button";
-
-export default function ProductSlider() {
+import "../Components/styles/hero.css"
+export default function AllProduct() {
   return (
-    <section className="flex justify-center items-center mt-[5rem] mb-[5rem]">
-      <div className="w-[100%] flex flex-col justify-center items-center">
+    <section className="flex flex-col gap-5 justify-center items-center ">
+        <div className="custom-bg w-full h-[12rem] flex items-end">
+            <p className="headline-three text-white m-[2rem]">All Products</p>
+        </div>
+      <div className="w-[100%] flex flex-col justify-center mb-[5rem] items-center">
         {/* Grid container for Product Tiles */}
         <div className="grid grid-cols-1 gap-6 w-[90%] sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {productDataAll.map((tile, index) => (
